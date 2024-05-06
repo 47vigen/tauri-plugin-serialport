008ba4c5e0609ad2459e9079aa50e17b46dbad6f
47vigen <47vigen@gmail.com>
2024-05-06 13:14:52 +0330

    feat: new version

--- src/lib.rs
+++ src/lib.rs
@@ -3,32 +3,62 @@ use tauri::{
     Manager, Runtime,
 };
 
-use api::*;
 use state::SerialPortState;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
 
-mod api;
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
 mod err;
+mod models;
 mod state;
+
+#[cfg(desktop)]
+use desktop::Serialport;
+#[cfg(mobile)]
+use mobile::Serialport;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the serialport APIs.
+pub trait SerialportExt<R: Runtime> {
+    fn serialport(&self) -> &Serialport<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::SerialportExt<R> for T {
+    fn serialport(&self) -> &Serialport<R> {
+        self.state::<Serialport<R>>().inner()
+    }
+}
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
     Builder::new("serialport")
         .invoke_handler(tauri::generate_handler![
-            available_ports,
-            cancel_read,
-            close,
-            close_all,
-            force_close,
-            open,
-            read,
-            write,
-            write_binary,
+            commands::available_ports,
+            commands::cancel_read,
+            commands::close,
+            commands::close_all,
+            commands::force_close,
+            commands::open,
+            commands::read,
+            commands::write,
+            commands::write_binary,
         ])
-        .setup(move |app, _webview| {
+        .setup(|app, api| {
+            #[cfg(mobile)]
+            let serialport = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let serialport: Serialport<R> = desktop::init(app, api)?;
+            app.manage(serialport);
+
+            // manage state so it is accessible by the commands
             app.manage(SerialPortState {
                 serialports: Arc::new(Mutex::new(HashMap::new())),
             });
